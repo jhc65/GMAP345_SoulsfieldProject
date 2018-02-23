45d40825c32e36c14aeb71efbe880f64feccab0a
Daniel Perlman <dmp359@drexel.edu>
2018-02-22 21:31:14 -0500

    Sound sound effects i made. They randomly play from ghosts every few seconds or so

---
+++ Week 3 Presentation Refactor/Assets/Scripts/PlayRandomSoundEffect.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayRandomSoundEffect : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] noises;
+    private AudioClip noise;
+
+    private float timeElapsed;
+    private float randomTimeForSound;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        randomTimeForSound = Random.Range(5, 15);
+    }
+    void Update()
+    {
+        if (timeElapsed >= randomTimeForSound)
+        {
+            int index = Random.Range(0, noises.Length);
+            audioSource.clip = noises[index];
+            audioSource.Play();
+            timeElapsed = 0;
+            randomTimeForSound = Random.Range(10, 30);
+        }
+        else {
+            timeElapsed += Time.deltaTime;
+            print(timeElapsed);
+        }
+    }
+}
