489e699a8f543d791c34fcc2a4b86989750bbc5b
TheOneTruBae <35905445+TheOneTruBae@users.noreply.github.com>
2018-03-15 02:27:52 -0400

    Removeable Trees
    Added a tree that functions like the door. Once the player enters a trigger area, it can interact with the tree and if the player has enough souls, then the tree will fade out. There needs to also have LeadToZone set or else the game will not function.

---
+++ Week 3 Presentation Refactor/assets/Scripts/TreeTriggerZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class is used to let the parent tree manager know when the player is in the trigger zone.
+*/
+public class TreeTriggerZone : MonoBehaviour {
+
+    private TreeManager parent;
+
+    void Start()
+    {
+        parent = GetComponentInParent<TreeManager>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            parent.OnChildTriggerEnter();
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            parent.OnChildTriggerExit();
+        }
+    }
+}
